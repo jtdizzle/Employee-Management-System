9121c5a85afd6a3d91bc2ca0c78f6376480e2c03
jonathan taylor <87590712+jtdizzle@users.noreply.github.com>
2022-01-20 01:16:08 -0500

    Create connection.js

---
+++ assets/connection.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql2");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employee_trackerDB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected id " + connection.threadId + "\n");
+});
+
+module.exports = connection;
